4579416f516420e69b43dd9d275617075731a2e3
Alfio Martini <alfio.martini@gmail.com>
2022-03-28 23:50:41 -0300

    Diminished resolution of Gallery's images (hobbies)

--- hobbies.js
+++ hobbies.js
@@ -1,10 +1,10 @@
 const hobbies = [
-  { image: "./images/hobbies_reading.jpg", text: "" },
-  { image: "./images/hobbies_reading02.jpg", text: "" },
-  { image: "./images/hobbies_runner.jpg", text: "" },
-  { image: "./images/hobbies_running.jpg", text: "" },
-  { image: "./images/hobbies_music01.jpg", text: "" },
-  { image: "./images/hobbies_music02.jpg", text: "" },
+  { image: "./images/hobbies_reading_wide800.jpg", text: "" },
+  { image: "./images/hobbies_reading02_wide800.jpg", text: "" },
+  { image: "./images/hobbies_runner_wide800.jpg", text: "" },
+  { image: "./images/hobbies_running_wide800.jpg", text: "" },
+  { image: "./images/hobbies_music01_wide800.jpg", text: "" },
+  { image: "./images/hobbies_music02_wide800.jpg", text: "" },
 ];
 
 export { hobbies };
